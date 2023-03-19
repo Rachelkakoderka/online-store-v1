af8b93284c82cef5751c3594ae3f90983494d2dc
Rachelkakoderka <rachelkakoderka@gmail.com>
2023-03-19 23:42:40 +0100

    total cost still not fixed

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -10,11 +10,18 @@ const Cart = () => {
     const cart:[string] = useSelector((store : StoreInterf) => store.itemsInCart);
     //console.log(cart)
 
-    const allItems = cart.map(id => products.find(x => x.id === id))
+    function allItems() {
+        let allItems;
+        if (cart.length > 0) {
+            return  allItems = cart.map(id => products.find(x => x.id === id))
                         .map(item => item?.price) 
+        } else {
+            return allItems = []
+        }
+    }
 
    console.log(allItems)
-    const totalCost : number = allItems.reduce(((acc, curr) => acc +=  curr), 0)
+    const totalCost = allItems().reduce(((acc, curr) => {return acc + curr}), 0)
     const cartItemElems = cart.map(thing => <CartItem id={thing}/>)
     console.log(totalCost)
 
