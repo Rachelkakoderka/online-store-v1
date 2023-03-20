87f2b029c204a5e57cfca05fee48b5f4b784fd03
Rachelkakoderka <rachelkakoderka@gmail.com>
2023-03-20 08:50:51 +0100

    cart btn

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -10,29 +10,45 @@ const Cart = () => {
     const cart:[string] = useSelector((store : StoreInterf) => store.itemsInCart);
     //console.log(cart)
 
-    function allItems() {
-        let allItems;
-        if (cart.length > 0) {
-            return  allItems = cart.map(id => products.find(x => x.id === id))
+  const  allItems = cart.map(id => products.find(x => x.id === id))
                         .map(item => item?.price) 
-        } else {
-            return allItems = []
-        }
+        
+    const totalCost = () : number => {
+            if (allItems.length > 0) {
+               return allItems.reduce((acc : number, curr? : number) => {
+               if (curr !== undefined) {
+                return acc+curr
+                }
+                return  acc}, 0)
+            }
+        return 0        
     }
+    
+    console.log(allItems)
+    
 
-   console.log(allItems)
-    const totalCost = allItems().reduce(((acc, curr) => {return acc + curr}), 0)
-    const cartItemElems = cart.map(thing => <CartItem id={thing}/>)
+    const cartItemElems = cart.map(thing => <CartItem key={thing} id={thing}/>)
     console.log(totalCost)
 
     return (
         <div className="cart__container">
             <div className="cart__elements">
                 {cart.length ? cartItemElems : "Your cart is empty"}
-                <div className="cart__total">
-                    <h2>Razem:</h2>
-                    <h3>{totalCost} PLN</h3>
-                </div>
+                { cart.length > 0 ?
+                    <div className="cart__counter">
+                        <div className="cart__total">
+                            <h2>Razem:</h2>
+                            <h2>{totalCost()} PLN</h2>
+                        </div>
+                        {/* //<div className="place_order__container"></div> */}
+                        <button className="place_order__btn">
+                            Złóż zamówienie
+                        </button>
+                    
+                    </div>
+                : ""
+                }
+                
                 
             </div>
             
